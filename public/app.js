e332b3251307db1c4e91af4cf6b6f24f0b02a785
Samuel Tesla <samuel.tesla@gmail.com>
2020-08-25 21:22:12 -0500

    rework javascript with alpinejs

--- public/app.js
+++ public/app.js
@@ -1,58 +1,26 @@
 var evetools = {}
 
-evetools.appOnReady = function() {
-  document.querySelectorAll('.navbar-burger').forEach(el => {
-    el.addEventListener('click', () => {
-      let targetId = el.dataset.target;
-      let target = document.getElementById(targetId);
-      el.classList.toggle('is-active');
-      target.classList.toggle('is-active');
-    });
-  });
+evetools.globalState = function() {
+  return {
+    burgerOpen: false,
+    loggedIn: false,
+    user: null,
 
-  fetch('/api/v1/currentUser').then(function(resp) {
-    return new Promise((resolve, reject) => {
-      if (resp.ok) {
-        resp.json().then(resolve);
-      } else {
-        reject();
-      }
-    });
-  }).then(function(user) {
-    let navEnd = evetools.template('navbar-end');
-    let avatarUrl = 'https://imageserver.eveonline.com/Character/' + user.characterID + '_32.jpg';
-    navEnd.querySelector('.avatar').src = avatarUrl;
-    navEnd.querySelector('.name').textContent = user.characterName;
-    document.querySelector('.navbar-menu').appendChild(navEnd);
-    return user
-  }).then(function(user) {
-    evetools.showView(window.location.hash, user);
-  }).catch(function() {
-    evetools.showView('');
-  }).finally(() => {
-    document.querySelector('body').classList.remove('hidden');
-  });
-}
-
-evetools.showView = function(hash, user) {
-  if (!user) return;
-
-  let viewFn = evetools.homeView
-  if (viewFn) {
-    let viewContainer = document.querySelector('main');
-    while (viewContainer.firstChild) {
-      viewContainer.firstChild.remove();
-    }
-    viewContainer.append(viewFn(user));
+    fetchCurrentUser() {
+      fetch('/api/v1/currentUser').
+        then(resp => {
+          if (!resp.ok) {
+            throw new Error('error fetching current user');
+          }
+          return resp.json();
+        }).
+        then(user => {
+          console.log(user);
+          this.user = user
+          this.user.avatarURL = 'https://imageserver.eveonline.com/Character/' + user.characterID + '_32.jpg';
+          this.loggedIn = true;
+        }).catch(() => {});
+    },
   }
 }
 
-evetools.template = function(name) {
-  return document.querySelector('.templates .' + name).cloneNode(true);
-}
-
-evetools.homeView = function(user) {
-  let view = evetools.template('view-home');
-  view.querySelector('.name').textContent = user.characterName;
-  return view;
-}
