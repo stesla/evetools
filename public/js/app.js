38d50c20870199a7c3b6e21cc3625d71d4db288a
Samuel Tesla <samuel.tesla@gmail.com>
2020-11-11 10:13:35 -0600

    sort favorites on home page

--- public/js/app.js
+++ public/js/app.js
@@ -186,6 +186,13 @@ evetools = (function(document, window, undefined) {
             type.favorite = true
             this.favorites.push(type)
           })
+          this.favorites.sort((a, b) => {
+            if (a.name < b.name)
+              return -1;
+            else if (a.name > b.name)
+              return 1;
+            return 0;
+          });
         })
       },
 
