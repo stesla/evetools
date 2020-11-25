4ab8ec2abb62bffaaccfec0c57bd28972ca461a8
Samuel Tesla <samuel.tesla@gmail.com>
2020-11-25 01:03:56 -0600

    refactor to have station have its own state object

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,75 +1,108 @@
 viewData = (function(window, document, undefined) {
   var data = retrieve('/api/v1/view/settings', 'error fetching data view');
+  var loaded = false;
+  data.then(() => { loaded = true; });
+
+  function settings() {
+    return {
+      characters: {},
+
+      initialize() {
+        document.title += " - Settings";
+        data.then(data => {
+          this.characters = data.characters;
+        });
+      },
+
+      get characterList() {
+        return Object.values(this.characters).sort(byName);
+      },
+
+      get loaded() {
+        return loaded;
+      },
+
+      makeActiveCharacter(cid) {
+        retrieve('/api/v1/user/characters/' + cid + '/activate', 'error activating user', {
+          raw: true,
+          method: 'POST',
+        })
+        .then(() => {
+          window.location.href = "/";
+        });
+      },
+
+    }
+  }
+
+  function stationList() {
+    return {
+      editing: false,
+      listOpen: false,
+      station: { name: "" },
+      stationName: "",
+      stationList: [],
+
+      initialize() {
+        data.then(data => {
+          this.station = data.station;
+        });
+      },
+
+      beginEdit(event, nextTick) {
+        this.editing = true
+        let input = event.target.parentElement.parentElement.parentElement.querySelector('input');
+        console.log(input);
+        nextTick(() => { input.focus(); });
+      },
+
+      fetch() {
+        if (this.stationName.length < 3) {
+          this.stationList = [];
+          return;
+        }
+        const params = new URLSearchParams();
+        params.set("q", this.stationName);
+        retrieve('/api/v1/stations?' + params.toString()).then(stationList => {
+          this.stationList = stationList.sort(byName);
+        });
+      },
+
+      get loaded() {
+        return loaded;
+      },
+
+      save() {
+        if (this.stationName === "") {
+          this.stationList = [];
+          this.editing = false;
+          return;
+        }
+        station = this.stationList.find(s => s.name === this.stationName);
+        return retrieve('/api/v1/user/station', 'error saving station', {
+          raw: true,
+          method: 'PUT',
+          body: JSON.stringify(station),
+        })
+        .then(() => {
+          this.station = station;
+          this.stationName = "";
+          this.stationList = [];
+          this.editing = false;
+        });
+      },
+
+      select(event, nextTick) {
+        this.stationName=event.target.value;
+        this.listOpen=false;
+        let button = event.target.parentElement.parentElement.querySelector('button');
+        nextTick(() => { button.focus(); });
+      },
+    };
+  }
 
   return {
-    characters: {},
-    editingStation: false,
-    station: { name: "" },
-    stationListOpen: false,
-    stationName: "",
-    stations: [],
-    loaded: false,
-
-    initialize() {
-      document.title += " - Settings";
-      data.then(data => {
-        this.characters = data.characters;
-        this.station = data.station;
-        this.loaded = true;
-      });
-    },
-
-    get characterList() {
-      return Object.values(this.characters).sort(byName);
-    },
-
-    fetchStations() {
-      if (this.stationName.length < 3) {
-        this.stations = [];
-        return;
-      }
-      const params = new URLSearchParams();
-      params.set("q", this.stationName);
-      retrieve('/api/v1/stations?' + params.toString()).then(stations => {
-        this.stations = stations.sort(byName);
-      });
-    },
-
-    makeActiveCharacter(cid) {
-      retrieve('/api/v1/user/characters/' + cid + '/activate', 'error activating user', {
-        raw: true,
-        method: 'POST',
-      })
-      .then(() => {
-        window.location.href = "/";
-      });
-    },
-
-    saveStation() {
-      if (this.stationName === "") {
-        this.stations = [];
-        this.editingStation = false;
-        return;
-      }
-      station = this.stations.find(s => s.name === this.stationName);
-      return retrieve('/api/v1/user/station', 'error saving station', {
-        raw: true,
-        method: 'PUT',
-        body: JSON.stringify(station),
-      })
-      .then(() => {
-        this.station = station;
-        this.stationName = "";
-        this.stations = [];
-        this.editingStation = false;
-      });
-    },
-
-    selectStation(event, nextTick) {
-      this.stationName=event.target.value;
-      this.stationListOpen=false;
-      let button = event.target.parentElement.parentElement.querySelector('button');
-      nextTick(() => { button.focus(); });
-    }
+    settings: settings,
+    stationList: stationList,
   }
 })(window, document, undefined);
